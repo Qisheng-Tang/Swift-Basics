bec37a60b03c789ad4fba7c511b08569586a2881
唐琪盛 <tangqisheng@guest-10-164-16-175.wireless.umn.edu>
2018-06-13 17:18:44 -0500

    Initial Commit

---
+++ Swift-Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift-Basics
+//
+//  Created by 唐琪盛 on 6/13/18.
+//  Copyright © 2018 Qisheng Tang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
